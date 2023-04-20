ddfaad4f967048dbaf5a0d7a53c8cddd0e6a6f98
HS H <3262398@naver.com>
2023-04-20 18:07:03 +0900

    0420

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../css/main.css'
+
+export default function Menu() {
+  return (
+    <div className='box'>
+        <div className='logo'></div>
+        <div className='nav'></div>
+    </div>
+  )
+}
---
+++ src/page/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../css/main.css'
+
+export default function Main() {
+  return (
+    <div>
+      <div className='headerImg'></div>
+      <div></div>
+      <div className='textBackground'>
+        <h1>Krispy<br/>Kreme</h1>
+      </div>
+      
+    </div>
+  )
+}
